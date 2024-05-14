1a480e059c81cf8782cc51083b79a8d480bcd977
dmj17b <dmj17b@fsu.edu>
2024-05-14 15:53:26 -0400

    Working, but motor names dont match hardware

--- src/main.cpp
+++ src/main.cpp
@@ -10,53 +10,52 @@ PulsePositionInput ppi;
 
 
 // Motor pin definitions
-// NOT CORRECT YET
-#define m1_EN 5
-#define m1_DIR 6
-#define m1_ENCA 2
-#define m1_ENCB 7
+#define leftHip_EN 5
+#define leftHip_DIR 6
+#define leftHip_ENCA 2
+#define leftHip_ENCB 7
 
-#define m2_EN 3
-#define m2_DIR 4
-#define m2_ENCA 0
-#define m2_ENCB 1
+#define leftKnee_EN 3
+#define leftKnee_DIR 4
+#define leftKnee_ENCA 0
+#define leftKnee_ENCB 1
 
-#define m3_EN 23
-#define m3_DIR 22
-#define m3_ENCA 18
-#define m3_ENCB 19
+#define rightHip_EN 23
+#define rightHip_DIR 22
+#define rightHip_ENCA 18
+#define rightHip_ENCB 19
 
-#define m4_EN 15
-#define m4_DIR 20
-#define m4_ENCA 16
-#define m4_ENCB 17
+#define rightKnee_EN 15
+#define rightKnee_DIR 20
+#define rightKnee_ENCA 16
+#define rightKnee_ENCB 17
 
 // I2C address for the legs:
 #define Master Serial6
 
 
 // Variables for holding desired motor positions:
-float m1DesPos = 0;
-float m2DesPos = 0;
-float m3DesPos = 0;
-float m4DesPos = 0;
+float leftHipDesPos = 0;
+float leftKneeDesPos = 0;
+float rightHipDesPos = 0;
+float rightKneeDesPos = 0;
 
 // Create a timer object to control the control loop
 PeriodicTimer controlTimer;
 
 // Create a motor object for the knee motor
-Motor m1 = Motor(m1_EN, m1_DIR, m1_ENCA, m1_ENCB);
-Motor m2 = Motor(m2_EN, m2_DIR, m2_ENCA, m2_ENCB);
-Motor m3 = Motor(m3_EN, m3_DIR, m3_ENCA, m3_ENCB);
-Motor m4 = Motor(m4_EN, m4_DIR, m4_ENCA, m4_ENCB);
+Motor leftHip = Motor(leftHip_EN, leftHip_DIR, leftHip_ENCA, leftHip_ENCB);
+Motor leftKnee = Motor(leftKnee_EN, leftKnee_DIR, leftKnee_ENCA, leftKnee_ENCB);
+Motor rightHip = Motor(rightHip_EN, rightHip_DIR, rightHip_ENCA, rightHip_ENCB);
+Motor rightKnee = Motor(rightKnee_EN, rightKnee_DIR, rightKnee_ENCA, rightKnee_ENCB);
 
 
 // Main control function to run every 5ms
 void controlFunc(){
-  m1.posControl(m1DesPos);
-  m2.posControl(m2DesPos);
-  m3.posControl(m3DesPos);
-  m4.posControl(m4DesPos);
+  leftHip.posControl(leftHipDesPos);
+  leftKnee.posControl(leftKneeDesPos);
+  rightHip.posControl(rightHipDesPos);
+  rightKnee.posControl(rightKneeDesPos);
 }
 
 
@@ -66,12 +65,20 @@ void setup()
 {
   Serial.begin(115200);            // Boot up the serial monitor
   Master.begin(9600);            // Boot up the serial monitor
-  m1.setGains(3.0, 0.0, 1.0);      // Set PID gains for m1
-  m2.setGains(3.0, 0.0, 1.0);      // Set PID gains for m2
-  m3.setGains(3.0, 0.0, 1.0);      // Set PID gains for m3
-  m4.setGains(3.0, 0.0, 1.0);      // Set PID gains for m4
+  leftHip.setGains(4.0, 0.25, 2.0);      // Set PID gains for leftHip
+  leftHip.setGearReduction((360*16)/(12*379.17*40));
+
+  leftKnee.setGains(4.0, 0.25, 2.0);      // Set PID gains for leftKnee
+  leftKnee.setGearReduction((360*20)/(12*379.17*48));
+
+  rightHip.setGains(4.0, 0.25, 2.0);      // Set PID gains for rightHip
+  rightHip.setGearReduction((360*16)/(12*379.17*40));
+
+  rightKnee.setGains(4.0, 0.25, 2.0);      // Set PID gains for rightKnee
+  rightKnee.setGearReduction((360*20)/(12*379.17*48));
+
+
   controlTimer.begin(controlFunc, 5000);  // Set the control function to run every 5ms
-  // Wire.onRequest(sendUpdate);
 
 }
 
@@ -79,39 +86,57 @@ int jointIndex;
 // Main loop
 void loop()
 {
-  controlTimer.start();
+
+  // Very first thing to do is to check if there is any new data from the master
   static float input;
   while(Master.available()>0){
+    // Parse data into jointIndex and input
     int d1 = Master.parseInt();
     input = Master.parseFloat();
+    Serial.println(d1);
     jointIndex = d1;
-    Serial.println(jointIndex);
-
     break;
   }
+
+  // Check if the jointIndex is 0, if so, kill all motors
+  if(jointIndex == 0){
+    leftHip.kill();
+    leftKnee.kill();
+    rightHip.kill();
+    rightKnee.kill();
+    controlTimer.stop();
+    Serial.println("Killed all motors");
+  }
+  else{
+    controlTimer.start();
+  }
+
+  // Either way, switch on the jointIndex to set the desired position of the motor
   switch(jointIndex){
     case 0:
-      m1.kill();
-      m2.kill();
-      m3.kill();
-      m4.kill();
-      controlTimer.stop();
+      leftHip.kill();
+      leftKnee.kill();
+      rightHip.kill();
+      rightKnee.kill();
       Serial.println("Killed all motors");
     case 1:
-      m1DesPos = input;
-      Serial.println(input);
+      leftHipDesPos = input;
       break;
     case 2:
-      m2DesPos = input;
+      leftKneeDesPos = input;
       break;
     case 3:
-      m3DesPos = input;
+      rightHipDesPos = input;
       break;
     case 4:
-      m4DesPos = input;
+      rightKneeDesPos = input;
       break;
     default:
-      controlTimer.stop();
+      leftHip.kill();
+      leftKnee.kill();
+      rightHip.kill();
+      rightKnee.kill();
+      Serial.println("Killed all motors");
       break;
   }
 
